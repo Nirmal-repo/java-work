def5f0efda5a3b94f208d353133612d40406b5b5
Nirmal <nirmal.1985@yahoo.com>
2021-04-14 09:38:40 +0530

    updated String class again with Welcome message

--- src/com/java/string/tutorial/WelcomeStringClass.java
+++ src/com/java/string/tutorial/WelcomeStringClass.java
@@ -4,5 +4,7 @@ public class WelcomeStringClass {
 
     public static void main(String args[]){
         System.out.println("Hello Welcome String Class");
+        System.out.println("Hello Mr. NIrmal Jain");
+
     }
 }
